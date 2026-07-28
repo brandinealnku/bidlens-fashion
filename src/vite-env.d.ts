d7cf6c056d811481b13f928a4d5267771b5c2cfc
Brandi Neal <nealb3@nku.edu>
2026-07-28 20:57:54 +0000

    Fix build and deploy BidLens to GitHub Pages

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,3 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_CAPTURE_API_URL?: string;
-  readonly VITE_CAPTURE_ENABLED?: string;
-  readonly VITE_BASE_PATH?: string;
-}
+declare module "*.css";
